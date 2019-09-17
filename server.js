4894204d1f16d6a0b6876b2b27c3226d196d0380
Henry Ly <henryly213@gmail.com>
2019-09-17 14:29:24 -0500

    fix port problem

--- server.js
+++ server.js
@@ -56,7 +56,9 @@ function createTextResponse(textResponse) {
   return response;
 }
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("Server has started at port 3000");
 });
 
